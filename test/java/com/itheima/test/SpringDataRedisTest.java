6b2b386533731b38711c0b0bfdcbcad69a8e56f0
wuqing <60786467+wuqingcanshang@users.noreply.github.com>
2023-04-14 13:12:24 +0800

    Add files via upload

---
+++ test/java/com/itheima/test/SpringDataRedisTest.java
@@ -0,0 +1,19 @@
+package com.itheima.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringDataRedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Test
+    public void testString(){
+        redisTemplate.opsForValue().set("city","beijing");
+    }
+}
